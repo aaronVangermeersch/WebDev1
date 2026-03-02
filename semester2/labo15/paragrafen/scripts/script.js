28513765c14178c3d9ce357948fd71bef88f6e3a
aaronVangermeersch <aaron.vangermeersch@student.vives.be>
2026-03-02 11:34:44 +0100

    labo15

---
+++ semester2/labo15/paragrafen/scripts/script.js
@@ -0,0 +1,9 @@
+const setup = () => {
+    const belangrijkeElementen = document.getElementsByClassName("belangrijk");
+
+    for (let i = 0; i < belangrijkeElementen.length; i++) {
+        belangrijkeElementen[i].className += " opvallend";
+        }
+}
+
+window.addEventListener("load", setup);
